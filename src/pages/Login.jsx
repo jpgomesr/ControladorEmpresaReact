f96264d0a102830c6d63e68ee0e51ba774a42d4a
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2024-09-24 18:33:06 -0300

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,8 +4,13 @@ import { useNavigate } from "react-router-dom";
 function Login() {
   const navigate = useNavigate();
   const [idFuncionario, setIdFuncionario] = useState("");
+  const [error, setError] = useState("");
 
   function onSeeDetailsClick() {
+    if (idFuncionario.trim() === "") {
+      setError("Id funcionário está vazio!");
+      return;
+    }
     navigate(`/machine?idfunc=${idFuncionario}`);
   }
 
@@ -15,11 +20,19 @@ function Login() {
         <p className="font-bold text-[2rem]">Login</p>
         <input
           value={idFuncionario}
-          onChange={(e) => setIdFuncionario(e.target.value)}
+          onChange={(e) => {
+            setIdFuncionario(e.target.value);
+            setError("");
+          }}
           className="rounded-xl py-1 px-2 text-[1.2rem]"
           type="number"
           placeholder="Id funcionário"
         />
+        {error && (
+          <p className="text-red-500 font-bold bg-gray-300 px-2 py-1 rounded-xl">
+            {error}
+          </p>
+        )}
         <button
           className="bg-white px-2 py-1 rounded-xl font-bold text-[1.2rem]"
           onClick={onSeeDetailsClick}
